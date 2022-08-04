6d7c87d32ad878450464a08cc540bad8aa9f04fb
Juancavallejo <93886815+Juancavallejo@users.noreply.github.com>
2022-08-04 12:11:42 -0500

    Small changes like props name and some comentaries

--- src/Componets/Navbar/Navbar.js
+++ src/Componets/Navbar/Navbar.js
@@ -23,14 +23,14 @@ const NavbarBoostrap = () => {
                         <Nav.Link href='/category/Desiertos'>Desiertos</Nav.Link>
                         <Nav.Link href='/category/Nieve'>Nevados</Nav.Link>
                         <Nav.Link href='/category/Selvas'>Selvas</Nav.Link>
-                        <NavDropdown title="Colombia" id="navbarScrollingDropdown">
+                        {/* <NavDropdown title="Colombia" id="navbarScrollingDropdown">
                             <NavDropdown.Item href="#action3">Desierto de la Tatacoa</NavDropdown.Item>
                             <NavDropdown.Item href="#action4"> Desierto de La Guajira </NavDropdown.Item>
                             <NavDropdown.Item href="#action4"> Amazonas </NavDropdown.Item>
                             <NavDropdown.Item href="#action4"> Rio de los 7 colores </NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action5"> Parque Nacional de los Nevados</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
                     </Nav>
                     <Form className="d-flex justify-content-end">
                         <div className="d-flex mx-4 mt-2"> 1 <FontAwesomeIcon icon={faCartShopping}/> </div>
