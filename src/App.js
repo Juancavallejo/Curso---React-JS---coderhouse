2e3bfa411aa63bd1fb8344d51fc801f9e680141a
Juancavallejo <93886815+Juancavallejo@users.noreply.github.com>
2022-08-05 17:20:08 -0500

    deleted counter

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
-/*   const handleAddAlCarrito = (cantidad) => {
-    alert (`la cantidad agregada es: ${cantidad}`)
-  } */
-
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,7 +17,6 @@ function App() {
         <Routes>
           <Route path = "/" element={<ItemListContainer mensajeBienvenida= "Hola, buscamos poder ofrecerte las mejores experiencias de la mano de valiosos recipientes de experiencias provenientes de los lugares más emblematicos." />}/>
           <Route path='/category/:categoryId' element={<ItemListContainer mensajeBienvenida= 'Listado filtrado'/>}/>
-          {/* <Route path = "/" element={ <Counter stock={10} addAlCarrito ={handleAddAlCarrito}/>}/> */}
           <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>  
         </Routes>
      </BrowserRouter>
