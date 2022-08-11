1773368af9a5cfb2c8c9930b786c08cc92f8d9d1
Juancavallejo <93886815+Juancavallejo@users.noreply.github.com>
2022-08-11 10:13:51 -0500

    Minor change on Item

--- src/Componets/Item/Item.js
+++ src/Componets/Item/Item.js
@@ -5,7 +5,7 @@ import "./Item.css";
 const Item = ({ product }) => {
     return (
             <Card border="dark" className='cardList d-inline-flex m-3 ' style={{ width: '28rem', height: '37rem' }}>
-                <Card.Img variant="top" src={product.img} />
+                <Card.Img variant="top" src={product?.img} />
                 <Card.Body>
                     <Card.Title>{product.name}</Card.Title>
                     <Card.Text className='text-start'>{product.description}</Card.Text>
